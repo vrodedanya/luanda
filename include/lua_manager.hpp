23d5d01dade107d0fe972f59041649ae819738bf
vrodedanya <socurahlem@yandex.ru>
2021-02-23 17:08:07 +0300

    Added lua_manager class

---
+++ include/lua_manager.hpp
@@ -0,0 +1,15 @@
+#ifndef LUA_MANAGER_HPP
+#define LUA_MANAGER_HPP
+
+#include "lua.hpp"
+
+namespace luanda
+{
+	class lua_manager
+	{
+	private:
+	public:
+	};
+}
+
+#endif
---
+++ source/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lua_manager.hpp"
+
+int main()
+{
+	luanda::lua_manager manager;
+	
+	return 0;
+}
